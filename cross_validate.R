900c708d3dbb50b74cdc36283ab9380e1281c7c5
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2016-10-16 19:34:22 +0200

    Update cross_validate.R
    Removed personal notes from the bottom of the script. That's going somewhere else ;)

--- cross_validate.R
+++ cross_validate.R
@@ -776,24 +776,5 @@ cross_validate_list = function(model_list, data, id_column, cat_column,
   
 }
 
-
-########################
-# Notes for development:
-#
-# 2. Create a sorting function that takes into account the rmse, AIC and whether there's any convergence errors
-# Maybe a parameter can be to rank by rmse/AIC/r2m etc. as it can be hard to do weighting of the different 
-# parameters that are on different scales, etc.
-# Some might prefer to have the models in the way they've built it up - (so they keep adding variables)
-#
-#
-# Question: Is it meaningful to have AIC from each fold and report a mean of that? 
-## Or should we compare to AIC on the model fitted on the entire dataset instead?
-#
-# Warning messages:
-#   1: In confusionMatrix.default(binomial_pred_obs_class$predicted_class,  :
-#                                   Levels are not in the same order for reference and data. Refactoring data to match.
-#
-# Are we supposed to always get the same prevalence?
-#
-########################
+# The end
 

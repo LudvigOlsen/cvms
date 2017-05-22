c107c44fe053fe5292b91d42f2b050a0c9a02a17
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-05-22 22:14:31 +0200

    Initialized testing of cv_plot. For now it only tests the objects class ("ggplot").

---
+++ tests/testthat/test_cv_plot.R
@@ -0,0 +1,31 @@
+library(cvms)
+context("cv_plot()")
+
+
+test_that("cv_plot() works",{
+
+  # Load data and fold it
+  set.seed(1)
+  dat <- groupdata2::fold(participant.scores, k = 4,
+                          cat_col = 'diagnosis',
+                          id_col = 'participant')
+
+  # Cross-validate the data
+  CVbinom <- cross_validate(dat, "diagnosis~score",
+                            folds_col = '.folds',
+                            family='binomial', REML = FALSE,
+                            model_verbose=FALSE)
+
+  CVgauss <- cross_validate(dat, "score~diagnosis",
+                            folds_col = '.folds',
+                            family='gaussian', REML = FALSE,
+                            model_verbose=FALSE)
+
+  expect_is(cv_plot(CVbinom, type = 'ROC'), 'ggplot')
+  expect_is(cv_plot(CVgauss, type = 'RMSE'), 'ggplot')
+  expect_is(cv_plot(CVgauss, type = 'r2'), 'ggplot')
+  expect_is(cv_plot(CVgauss, type = 'IC'), 'ggplot')
+  expect_is(cv_plot(CVgauss, type = 'coefficients'), 'ggplot')
+
+})
+

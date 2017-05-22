0f6f5ebddcce59e6607d4479c23d508aa05ef0ac
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-05-23 00:40:44 +0200

    Documented dataset participant.scores

---
+++ R/data_documentation.R
@@ -0,0 +1,13 @@
+#' Participant scores
+#'
+#' 10 participants. 5 for each of 2 diagnoses.
+#' Scores for 3 sessions per participant. Participants get better for each session.
+#' Age of participants.
+#'
+#' Simulated data. 30 rows. 5 columns.
+#'
+#' @name participant.scores
+#' @docType data
+#' @author Ludvig Renbo Olsen, \email{r-pkgs@ludvigolsen.dk}
+#' @keywords data
+NULL

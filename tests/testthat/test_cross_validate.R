e0472eca8fc1cdd60e65f72ab4a70b513c983459
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-05-22 03:10:02 +0200

    Tested 1 gaussian model and 1 binomial model. Need to test more combinations of parameters.

--- tests/testthat/test_cross_validate.R
+++ tests/testthat/test_cross_validate.R
@@ -4,6 +4,7 @@ context("cross_validate()")
 test_that("gaussian models with cross_validate()",{
 
   # Load data and fold it
+  set.seed(1)
   dat <- groupdata2::fold(participant.scores, k = 4,
                           cat_col = 'diagnosis',
                           id_col = 'participant')
@@ -11,15 +12,53 @@ test_that("gaussian models with cross_validate()",{
   # Cross-validate the data
   CVed <- cross_validate(dat, "score~diagnosis", folds_col = '.folds', family='gaussian', REML = FALSE,
                          do.plot=TRUE, which_plot = "all",
-                         model_verbose=TRUE)
+                         model_verbose=FALSE)
 
-  #print(CVed)
+  expect_equal(CVed$RMSE, 17.16817, tolerance=1e-3)
+  expect_equal(CVed$r2m, 0.2640793, tolerance=1e-3)
+  expect_equal(CVed$r2c, 0.2640793, tolerance=1e-3)
+  expect_equal(CVed$AIC, 194.6904, tolerance=1e-3)
+  expect_equal(CVed$AICc, 195.9963, tolerance=1e-3)
+  expect_equal(CVed$BIC, 198.0243, tolerance=1e-3)
+  expect_equal(CVed$Folds, 4)
+  expect_equal(CVed$`Convergence Warnings`, 0)
+  expect_is(CVed$Plots$RMSE, "ggplot")
+  expect_is(CVed$Plots$r2, "ggplot")
+  expect_is(CVed$Plots$IC, "ggplot")
+  expect_is(CVed$Plots$coefficients, "ggplot")
 
 
-  # data, model, folds_col = '.folds', family='gaussian', REML=FALSE,
-  # cutoff=0.5, positive=1, do.plot=FALSE,
-  # ){
+})
 
+test_that("binomial models with cross_validate()",{
+
+  # Load data and fold it
+  set.seed(1)
+  dat <- groupdata2::fold(participant.scores, k = 4,
+                          cat_col = 'diagnosis',
+                          id_col = 'participant')
+
+  # Cross-validate the data
+  CVbinom <- cross_validate(dat, "diagnosis~score", folds_col = '.folds', family='binomial', REML = FALSE,
+                         do.plot=TRUE, which_plot = "all",
+                         model_verbose=FALSE)
+
+  expect_equal(CVbinom$AUC, 0.7615741, tolerance=1e-3)
+  expect_equal(CVbinom$`Lower CI`, 0.5851154, tolerance=1e-3)
+  expect_equal(CVbinom$`Upper CI`, 0.9380328, tolerance=1e-3)
+  expect_equal(CVbinom$kappa, 0.4927536, tolerance=1e-3)
+  expect_equal(CVbinom$Sensitivity, 0.5833333, tolerance=1e-3)
+  expect_equal(CVbinom$Specificity, 0.8888889, tolerance=1e-3)
+  expect_equal(CVbinom$`Pos Pred Value`, 0.7777778, tolerance=1e-3)
+  expect_equal(CVbinom$`Neg Pred Value`, 0.7619048, tolerance=1e-3)
+  expect_equal(CVbinom$F1, 0.6666667, tolerance=1e-3)
+  expect_equal(CVbinom$Prevalence, 0.4, tolerance=1e-3)
+  expect_equal(CVbinom$`Detection Rate`, 0.2333333, tolerance=1e-3)
+  expect_equal(CVbinom$`Detection Prevalence`, 0.3, tolerance=1e-3)
+  expect_equal(CVbinom$`Balanced Accuracy`, 0.7361111, tolerance=1e-3)
+  expect_equal(CVbinom$Folds, 4)
+  expect_equal(CVbinom$`Convergence Warnings`, 0)
+  expect_is(CVbinom$ROC_plot, "ggplot")
 
 
 })

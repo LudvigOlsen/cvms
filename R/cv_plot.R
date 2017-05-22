ff2ac7137aac42812a33577a4d13d969d33ffd41
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-05-22 16:41:12 +0200

    Moved plotting to its own function.

---
+++ R/cv_plot.R
@@ -0,0 +1,58 @@
+cv_plot <- function(x, type){
+
+  if (x$Family == 'gaussian'){
+
+    if (type == 'RMSE'){
+
+      x_ <- data.frame("RMSE" = x$results[[1]]$RMSE)
+
+      return(create_boxplot_(x_, 1))
+
+    }
+
+    if (type == 'r2'){
+
+      x_ <- data.frame("r2m" = x$results[[1]]$r2m,
+                       "r2c" = x$results[[1]]$r2c)
+
+      return(create_boxplot_(x_, 1, 2))
+
+    }
+
+    if (type == 'IC'){
+
+      x_ <- data.frame("AIC" = x$results[[1]]$AIC,
+                       "AICc" = x$results[[1]]$AICc,
+                       "BIC" = x$results[[1]]$BIC)
+
+      return(create_boxplot_(x_, 1, 3))
+
+    }
+
+    if (type == 'coefficients'){
+
+      return(ggplot2::ggplot(x$coefficients[[1]], ggplot2::aes(term, estimate)) +
+        ggplot2::geom_boxplot() +
+        ggplot2::labs(x = 'Fixed Effects', y = 'Estimate'))
+
+    }
+
+  } else if (x$Family == 'binomial') {
+
+    if (type == 'ROC'){
+
+      ROC_plot <- data.frame(y=x$ROC[[1]]$sensitivities, x=x$ROC[[1]]$specificities) %>%
+        ggplot2::ggplot(ggplot2::aes(x, y)) +
+        ggplot2::geom_line() +
+        ggplot2::scale_x_reverse() +
+        ggplot2::geom_abline(intercept=1, slope=1, linetype="dashed") +
+        ggplot2::xlab("Specificity") +
+        ggplot2::ylab("Sensitivity")
+
+      return(ROC_plot)
+
+    }
+  }
+
+
+}

27e9af8fb6cca5d8cb3e244cf9719d76cf01fb2d
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-05-22 19:06:51 +0200

    Tested convergence warnings

--- tests/testthat/test_cross_validate.R
+++ tests/testthat/test_cross_validate.R
@@ -10,7 +10,9 @@ test_that("gaussian models with cross_validate()",{
                           id_col = 'participant')
 
   # Cross-validate the data
-  CVed <- cross_validate(dat, "score~diagnosis", folds_col = '.folds', family='gaussian', REML = FALSE,
+  CVed <- cross_validate(dat, "score~diagnosis",
+                         folds_col = '.folds',
+                         family='gaussian', REML = FALSE,
                          model_verbose=FALSE)
 
   expect_equal(CVed$RMSE, 17.16817, tolerance=1e-3)
@@ -34,8 +36,10 @@ test_that("binomial models with cross_validate()",{
                           id_col = 'participant')
 
   # Cross-validate the data
-  CVbinom <- cross_validate(dat, "diagnosis~score", folds_col = '.folds', family='binomial', REML = FALSE,
-                         model_verbose=FALSE)
+  CVbinom <- cross_validate(dat, "diagnosis~score",
+                            folds_col = '.folds',
+                            family='binomial', REML = FALSE,
+                            model_verbose=FALSE)
 
   expect_equal(CVbinom$AUC, 0.7615741, tolerance=1e-3)
   expect_equal(CVbinom$`Lower CI`, 0.5851154, tolerance=1e-3)
@@ -53,6 +57,20 @@ test_that("binomial models with cross_validate()",{
   expect_equal(CVbinom$Folds, 4)
   expect_equal(CVbinom$`Convergence Warnings`, 0)
 
+
+})
+
+test_that("convergence errors count works cross_validate()",{
+
+  # Load data and fold it
+  set.seed(1)
+  dat <- groupdata2::fold(participant.scores, k = 4,
+                          cat_col = 'diagnosis',
+                          id_col = 'participant')
+
+  expect_warning(cross_validate(dat, "score~diagnosis*age*session + (1+diagnosis|session) + (1+diagnosis|age)",
+                         folds_col = '.folds',
+                         family='gaussian'), "cross_validate(): Convergence Warning:", fixed = TRUE)
 
 })
 

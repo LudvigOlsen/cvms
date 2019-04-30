45c0032635e1d5842b61ac27f1b153d600aac734
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2019-04-30 03:51:32 +0200

    init ensemble branch

---
+++ R/ensemble.R
@@ -0,0 +1,25 @@
+
+# What if "models" can both be formulas and trained model objects?
+
+# This should be object oriented!
+
+ensemble <- function(data, models, folds_col = '.folds', family='gaussian',
+                     link = NULL, control=NULL, REML=FALSE,
+                     cutoff=0.5, positive=1, rm_nc = FALSE, model_verbose=FALSE){
+
+
+  return( ensemble_list(data = data,
+                        model_list = models,
+                        folds_col = folds_col,
+                        family = family,
+                        link = link,
+                        control=control,
+                        REML = REML,
+                        cutoff = cutoff,
+                        positive = positive,
+                        rm_nc = rm_nc,
+                        model_verbose = model_verbose))
+
+
+
+}

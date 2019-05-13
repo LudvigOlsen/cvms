f77b98501370fca589c7903a89d95e81a0182f8a
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2019-05-14 00:04:00 +0200

    Cleans up combine_predictors. Still exponential exec time with n predictors.
    Need to find a quicker approach.

--- R/combine_predictors.R
+++ R/combine_predictors.R
@@ -1,7 +1,10 @@
 
 #' Create model formulas with all combinations of the fixed effects
 #' Random effects will be added as supplied
-combine_predictors <- function(dependent, fixed_effects, random_effects=NULL, max_interactions=NULL){
+combine_predictors <- function(dependent, fixed_effects,
+                               random_effects=NULL,
+                               max_fixed_effects=NULL,
+                               max_interaction_size=NULL){
 
   # Check inputs
   if (is.null(dependent)){
@@ -12,15 +15,28 @@ combine_predictors <- function(dependent, fixed_effects, random_effects=NULL, ma
     stop("Please specify vector/list of fixed_effects.")
   }
 
-  if (!is.null(max_interactions) && !is.numeric(max_interactions)){
+  if (!is.null(max_interaction_size) && !is.numeric(max_interaction_size)){
     stop("max_interactions must be scalar or NULL.")
   }
 
-  if (!is.null(random_effects) && !is.character(random_effects)){
-    stop("random_effects must be either a string or NULL. Example: '(1|x)'")
+  if (!is.null(max_fixed_effects) && !is.numeric(max_fixed_effects)){
+    stop("max_fixed_effects must be scalar or NULL.")
   }
 
-  n_fixed_effects <- length(fixed_effects)
+  if (!is.null(random_effects) && !is.character(random_effects)){
+    stop("random_effects must be either a string or NULL. Example: '(1|x)'.")
+  }
+
+  if (!is.character(fixed_effects)){
+    stop("fixed_effects must be of type character.")
+  }
+
+  # Find number of fixed effects
+  if (!is.null(max_fixed_effects)){
+    n_fixed_effects <- max_fixed_effects
+  } else {
+    n_fixed_effects <- length(fixed_effects)
+  }
 
   effect_combinations <- plyr::ldply(1:n_fixed_effects, function(i){
     as.data.frame(t(combn(fixed_effects, i)), stringsAsFactors=FALSE)
@@ -32,66 +48,77 @@ combine_predictors <- function(dependent, fixed_effects, random_effects=NULL, ma
     as.data.frame(stringsAsFactors=FALSE) %>%
     dplyr::distinct()
 
-  if (!is.null(max_interactions)){
+  if (!is.null(max_interaction_size)){
     operator_combinations <-  operator_combinations %>%
-      dplyr::mutate(n_interactions = rowSums(.=="*")) %>%
-      dplyr::filter(.data$n_interactions <= max_interactions) %>%
+      dplyr::mutate(n_interactions = purrr::pmap_dbl(., get_max_nway_interaction)) %>%
+      dplyr::filter(.data$n_interactions <= max_interaction_size) %>%
       dplyr::select(-.data$n_interactions)
   }
 
-  fixed_effects <- effect_combinations %>%
-    map_all_rows(create_formulas, operator_combinations) %>%
-    dplyr::pull(.data$Result) %>%
-    unlist() %>%
-    tibble::enframe(name=NULL) %>%
-    dplyr::mutate(NAs = stringr::str_count(.data$value, "__NA__"),
-                  value = stringr::str_sub(.data$value, 1, ifelse(NAs > 0, -9*NAs, -1)),
-                  value = trimws(.data$value, "b")) %>%
-    dplyr::pull(.data$value) %>%
-    unique()
+  formulas <- purrr::pmap_df(effect_combinations, create_formulas,
+                             prepared_operators=prepare_operators(operator_combinations)) %>%
+    dplyr::mutate(NAs = stringr::str_count(.data$Formula, "__NA__"),
+                  Formula = stringr::str_sub(.data$Formula, 1, ifelse(NAs > 0, -9*NAs, -1)),
+                  Formula = trimws(.data$Formula, "b")) %>%
+                  dplyr::pull(.data$Formula) %>%
+                  unique()
 
   if (is.null(random_effects)){
-    return( paste0(dependent, " ~ ", fixed_effects) )
+    return( paste0(dependent, " ~ ", formulas) )
   } else {
-    return( paste0(dependent, " ~ ", fixed_effects, " + ", random_effects) )
+    return( paste0(dependent, " ~ ", formulas, " + ", random_effects) )
   }
-
 
 }
 
-create_formulas <- function(effects, operators){
+prepare_operators <- function(operators){
+  operators$extra <- " "
+  as.data.frame(t(operators), stringsAsFactors=FALSE)
+}
 
-  if (is.data.frame(effects) && ncol(effects) > 1){
-    effects <- effects %>% t() %>% .[,1]
-    names(effects) <- NULL  # use unname() ?
+create_formulas <- function(..., prepared_operators){
+
+  effects <- unname(c(...))
+
+  if (nrow(prepared_operators) - length(effects) != 0){
+    stop("Number of columns in prepared_operators should be length(effects).")
   }
 
-  if (ncol(operators) - length(effects) == -1){
-    operators$extra <- " "
-  } else { stop("Number of columns in operators should be length(effects)-1.")}
-
-  formulas <- as.data.frame(t(operators), stringsAsFactors=FALSE) %>%
-    # dplyr::as_tibble() %>%
+  formulas <- prepared_operators %>%
     dplyr::mutate("effects_" = effects, "effect_index"=1:dplyr::n()) %>%
     dplyr::select(.data$effects_, dplyr::everything()) %>%
     tidyr::gather(key="combi",value="op", -dplyr::one_of("effects_", "effect_index")) %>%
     dplyr::mutate(pasted = paste0(.data$effects_," ",op," ")) %>%
     groupdata2::group(n=length(effects), method = "greedy") %>%
     dplyr::summarise(formula_ = trimws(paste0(.data$pasted, collapse = ""), "r")) %>%
-    dplyr::pull(.data$formula_)
+    dplyr::pull(.data$formula_) %>%
+    tibble::enframe(name=NULL, value="Formula")
 
   formulas
 }
 
-# https://stackoverflow.com/questions/46850278/dplyr-mutate-how-do-i-pass-one-row-as-a-function-argument
-map_all_rows <- function(data, func, ...){
-  data %>%
-    dplyr::rowwise() %>%
-    dplyr::do( X = as.data.frame(.) ) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate( Result = purrr::map(X, func, ...) )
+# # https://stackoverflow.com/questions/46850278/dplyr-mutate-how-do-i-pass-one-row-as-a-function-argument
+# map_all_rows <- function(data, func, ...){
+#   data %>%
+#     dplyr::rowwise() %>%
+#     dplyr::do( X = as.data.frame(.) ) %>%
+#     dplyr::ungroup() %>%
+#     dplyr::mutate( Result = purrr::map(X, func, ...) )
+# }
+
+# Finds the largest interaction
+# Example:
+#   get_max_nway_interaction(c("*","+","+","+","+","*","*","+","*","*","*")))
+#   returns: 3
+# ... are all the operators as arguments
+# Because that works with pmap
+get_max_nway_interaction <- function(...){
+  ops <- unname(c(...))
+
+  if ("*" %ni% ops) return(0)
+
+  rle_ <- rle(ops)
+  max(rle_$lengths[rle_$values == "*"])
 }
-
-
 
 
--- tests/testthat/test_combine_predictors.R
+++ tests/testthat/test_combine_predictors.R
@@ -12,23 +12,23 @@ test_that("predictors are properly combined with combine_predictors()",{
   fx <- c("a","b","c","d")
   rfx <- "(1|e)"
 
-  formulas_1 <- combine_predictors(dep, fx, random_effects = rfx, max_interactions = NULL)
+  formulas_1 <- combine_predictors(dep, fx, random_effects = rfx, max_interaction_size = NULL)
   expect_equal(length(formulas_1), 40)
   expect_equal(nchar(paste0(formulas_1, collapse=" ; ")), 909)
 
-  formulas_2 <- combine_predictors(dep, fx, random_effects = rfx, max_interactions = 3)
+  formulas_2 <- combine_predictors(dep, fx, random_effects = rfx, max_interaction_size = 3)
   expect_equal(length(formulas_2), 40)
   expect_equal(nchar(paste0(formulas_2, collapse=" ; ")), 909)
 
-  formulas_3 <- combine_predictors(dep, fx, random_effects = rfx, max_interactions = 2)
+  formulas_3 <- combine_predictors(dep, fx, random_effects = rfx, max_interaction_size = 2)
   expect_equal(length(formulas_3), 39)
   expect_equal(nchar(paste0(formulas_3, collapse=" ; ")), 881)
 
-  formulas_4 <- combine_predictors(dep, fx, random_effects = rfx, max_interactions = 1)
-  expect_equal(length(formulas_4), 32)
-  expect_equal(nchar(paste0(formulas_4, collapse=" ; ")), 701)
+  formulas_4 <- combine_predictors(dep, fx, random_effects = rfx, max_interaction_size = 1)
+  expect_equal(length(formulas_4), 33)
+  expect_equal(nchar(paste0(formulas_4, collapse=" ; ")), 729)
 
-  formulas_5 <- combine_predictors(dep, fx, random_effects = rfx, max_interactions = 0)
+  formulas_5 <- combine_predictors(dep, fx, random_effects = rfx, max_interaction_size = 0)
   expect_equal(length(formulas_5), 15)
   expect_equal(nchar(paste0(formulas_5, collapse=" ; ")), 305)
   expect_equal(formulas_5,
@@ -37,20 +37,44 @@ test_that("predictors are properly combined with combine_predictors()",{
                  "y ~ b + d + (1|e)","y ~ c + d + (1|e)","y ~ a + b + c + (1|e)","y ~ a + b + d + (1|e)",
                  "y ~ a + c + d + (1|e)","y ~ b + c + d + (1|e)","y ~ a + b + c + d + (1|e)"))
 
-  formulas_6 <- combine_predictors(dep, fx, random_effects = NULL, max_interactions = NULL)
+  formulas_6 <- combine_predictors(dep, fx, random_effects = NULL, max_interaction_size = NULL)
   expect_equal(length(formulas_6), 40)
   expect_equal(nchar(paste0(formulas_6, collapse=" ; ")), 589)
 
-  formulas_7 <- combine_predictors(dep, fx, random_effects = NULL, max_interactions = 2)
+  formulas_7 <- combine_predictors(dep, fx, random_effects = NULL, max_interaction_size = 2)
   expect_equal(length(formulas_7), 39)
   expect_equal(nchar(paste0(formulas_7, collapse=" ; ")), 569)
 
-  expect_error(combine_predictors(dep, NULL, random_effects = NULL, max_interactions = NULL),
+  expect_error(combine_predictors(dep, NULL, random_effects = NULL, max_interaction_size = NULL),
                "Please specify vector/list of fixed_effects.")
 
-  expect_error(combine_predictors(NULL, fx, random_effects = NULL, max_interactions = NULL),
+  expect_error(combine_predictors(NULL, fx, random_effects = NULL, max_interaction_size = NULL),
                "Please specify dependent variable.")
 
+  # system.time({
+  #   combine_predictors("y", as.character(1:8), random_effects = NULL, max_interaction_size = NULL)
+  # })
+
+  # data.frame("t"=c(0.132,0.312,0.836,2.199,6.903,26.337,103.696),
+  #            "n"=c(4,5,6,7,8,9,10)) %>%
+  #   ggplot(aes(x=n,y=t)) +
+  #   geom_line()
+
+
+  # # length(
+  # combine_predictors("y", as.character(1:8), random_effects = NULL, max_interaction_size = 3)
+  #
+  # data.frame("V1"=c("+","+","+"),
+  #            "V2"=c("*","+","*"),
+  #            "V3"=c("*","*","*"),
+  #            "V4"=c("+","+","*"),
+  #            "V5"=c("*","*","+"),
+  #            stringsAsFactors = FALSE) %>%
+  #   mutate( n_int = purr::pmap_dbl(., get_max_nway_interaction))
+
+
 })
+
+
 
 

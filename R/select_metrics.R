0bd7d5dc80a549518418e0531e0ab1220268cd14
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2019-05-17 01:07:59 +0200

    Init select_metrics for use in tutorials, etc.

---
+++ R/select_metrics.R
@@ -0,0 +1,32 @@
+select_metrics <- function(results){
+
+  # TODO Add checks of results input
+
+  if ("Random" %in% colnames(results)){
+    model_formula_cols <- c("Dependent", "Fixed", "Random")
+  } else {
+    model_formula_cols <- c("Dependent", "Fixed")
+  }
+
+
+  # What about "Convergence Warnings"? People should be aware of this!
+
+  if (results[["Family"]] == "gaussian"){
+
+    metric_cols <- c("RMSE", "MAE", "r2m", "r2c", "AIC", "AICc", "BIC")
+
+    return( dplyr::select(results, dplyr::one_of(metric_cols), dplyr::one_of(model_formula_cols)))
+
+  } else if (results[["Family"]] == "binomial"){
+
+    metric_cols <- c("Balanced Accuracy","F1","Sensitivity","Specificity","Pos Pred Value",
+                     "Neg Pred Value","AUC","Lower CI","Upper CI","Kappa",
+                     "MCC","Detection Rate","Detection Prevalence","Prevalence")
+
+    return( dplyr::select(results, dplyr::one_of(metric_cols), dplyr::one_of(model_formula_cols)))
+
+  } else {
+    stop(paste0("Family, ",results[["Family"]],", not currently supported."))
+  }
+
+}

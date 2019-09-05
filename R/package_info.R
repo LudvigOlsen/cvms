4cb124330a5044991ef1610b9ba2c9c577bc7574
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2019-09-05 21:02:42 +0200

    docs

--- R/package_info.R
+++ R/package_info.R
@@ -2,11 +2,12 @@
 #'
 #' Perform (repeated) cross-validation on a list of model formulas. Validate the best model on a validation set.
 #' Perform baseline evaluations on your test set. Generate model formulas by combining your fixed effects.
+#' Evaluate predictions from an external model.
 #'
 #' Returns results in a tibble for easy comparison, reporting and further analysis.
 #'
-#' The cvms package provides 3 main functions:
-#' \code{cross_validate}, \code{validate}, \code{baseline}.
+#' The cvms package provides 4 main functions:
+#' \code{cross_validate}, \code{validate}, \code{baseline}, and \code{evaluate}.
 #'
 #' And a couple of helper functions:
 #' \code{combine_predictors}, \code{select_metrics}, \code{reconstruct_formulas}, \code{cv_plot}.

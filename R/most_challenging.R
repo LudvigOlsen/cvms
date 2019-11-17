1e70ec8499931f0027ea75f23deb04871a305d7e
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2019-11-17 23:25:52 +0100

    Exports most_challenging() for finding obs. most difficult to predict

--- R/most_challenging.R
+++ R/most_challenging.R
@@ -1,39 +1,189 @@
-# TODO Perhaps rename now that it is based on a threshold instead of a relative comparison?
-# threshold percentage it should be wrongly predicted to be included
+#' @title Find the data points that were hardest to predict
+#' @aliases hardest
+#' @description
+#'  \Sexpr[results=rd, stage=render]{lifecycle::badge("experimental")}
+#'  Finds the data points that, overall, were the most challenging to predict,
+#'  based on a prediction score (\code{RMSE} for regression and
+#'  \code{Accuracy} for classification).
+#'
+#'
+#' @param data Data frame with predictions, targets and observation IDs.
+#'
+#'  Can be grouped by \code{\link[dplyr:group_by()]{dplyr::group_by()}}.
+#' @param obs_id_col Name of column with observation IDs. This will be used to aggregate
+#'  the performance of each observation.
+#' @param target_col Name of column with the true classes/values in \code{data}.
+#' @param prediction_col Name of column with the predictions.
+#'
+#'  When evaluating a classification task, the column should contain the predicted classes.
+#' @param type Type of task used to get the predictions:
+#'
+#'  \code{"gaussian"} for regression (like linear regression).
+#'
+#'  \code{"binomial"} for binary classification.
+#'
+#'  \code{"multinomial"} for multiclass classification.
+#' @param threshold Threshold to filter observations by. Depends on \code{type} and \code{threshold_is}.
+#'
+#'  The \code{threshold} can either be a \strong{percentage} or a \strong{score}.
+#'  For percentages, a lower \code{threshold}
+#'  returns fewer observations. For scores, this depends on \code{type}.
+#'
+#'  \subsection{"gaussian"}{
+#'  \subsection{threshold_is "percentage"}{
+#'  Percentage of the observations with the largest root mean square errors
+#'  to return.
+#'  }
+#'  \subsection{threshold_is "score"}{
+#'  Observations with a root mean square error larger than the \code{threshold} will be returned.
+#'  }
+#'  }
+#'  \subsection{"binomial", "multinomial"}{
+#'  \subsection{threshold_is "percentage"}{
+#'  Percentage of the observations with the lowest accuracies to return.
+#'  }
+#'  \subsection{threshold_is "score"}{
+#'  Observations with an accuracy below the threshold will be returned.
+#'  }
+#'  }
+#' @param threshold_is Either \code{"score"} or \code{"percentage"}. See \code{threshold}.
+#' @author Ludvig Renbo Olsen, \email{r-pkgs@@ludvigolsen.dk}
+#' @export
+#' @examples
+#' \donttest{
+#' # Attach packages
+#' library(cvms)
+#' library(groupdata2) # partition()
+#' library(dplyr) # %>% arrange()
+#' library(tibble)
+#'
+#' # Data is part of cvms
+#' data <- participant.scores
+#'
+#' # Set seed for reproducibility
+#' set.seed(1)
+#' }
 most_challenging <- function(data,
-                             obs_col = "Observation",
-                             targets_col = "Target",
-                             predictions_col = "Predicted Class", # TODO Should depend on family?
-                             family = "binomial",
-                             threshold = 0.85){
+                             obs_id_col = "Observation",
+                             target_col = "Target",
+                             prediction_col = dplyr::case_when(
+                               type == "gaussian" ~ "Prediction",
+                               TRUE ~ "Predicted Class"
+                             ),
+                             type = "binomial",
+                             threshold = 0.15,
+                             threshold_is = "percentage"){
 
   # If the dataset is grouped, we need the indices and keys for the groups
   # so we can evaluate group wise
   # grouping_factor <- dplyr::group_indices(data)
   grouping_keys <- dplyr::group_keys(data)
+  data <- dplyr::ungroup(data)
 
-  ### CLASSIFICATION
+  if (threshold_is == "percentage" && !is_between_(threshold, 0.0, 1.0))
+    stop("when 'threshold_is' a percentage, 'threshold' must be between 0 and 1.")
 
-  if (family %in% c("binomial", "multinomial")){
-    # Ensure same type
-    data[[targets_col]] <- as.character(data[[targets_col]])
-    data[[predictions_col]] <- as.character(data[[predictions_col]])
-
-    data[["Correct"]] <- data[[predictions_col]] == data[[targets_col]]
-    data[["Incorrect"]] <- data[[predictions_col]] != data[[targets_col]]
-
-    by_observation <- data %>%
-      dplyr::group_by_at(c(colnames(grouping_keys), obs_col)) %>%
-      dplyr::summarise(Correct = sum(.data$Correct),
-                       Incorrect = sum(.data$Incorrect),
-                       `Percentage Correct` = .data$Correct/dplyr::n())
-
-    return(
-      by_observation[by_observation[["Percentage Correct"]] < (1-threshold),]
+  if (type %in% c("binomial", "multinomial")) {
+    most_challenging_classification(
+      data = data,
+      obs_id_col = obs_id_col,
+      target_col = target_col,
+      prediction_col = prediction_col,
+      threshold = threshold,
+      threshold_is = threshold_is,
+      grouping_keys = grouping_keys
     )
 
-  } else if (family == "gaussian"){
-    stop("Not yet implemented!")
+  } else if (type == "gaussian") {
+    most_challenging_gaussian(
+      data = data,
+      obs_id_col = obs_id_col,
+      target_col = target_col,
+      prediction_col = prediction_col,
+      threshold = threshold,
+      threshold_is = threshold_is,
+      grouping_keys = grouping_keys
+    )
+
   }
+
+}
+
+most_challenging_classification <- function(data,
+                                            obs_id_col,
+                                            target_col,
+                                            prediction_col,
+                                            threshold,
+                                            threshold_is,
+                                            grouping_keys) {
+  # Ensure same type
+  data[[target_col]] <- as.character(data[[target_col]])
+  data[[prediction_col]] <- as.character(data[[prediction_col]])
+
+  data[["Correct"]] <- data[[prediction_col]] == data[[target_col]]
+  data[["Incorrect"]] <- data[[prediction_col]] != data[[target_col]]
+
+  by_observation <- data %>%
+    dplyr::group_by_at(c(colnames(grouping_keys), obs_id_col)) %>%
+    dplyr::summarise(Correct = sum(.data$Correct),
+                     Incorrect = sum(.data$Incorrect),
+                     Accuracy = .data$Correct/dplyr::n())
+
+  if (threshold_is == "score"){
+
+    to_return <- by_observation[by_observation[["Accuracy"]] < threshold,]
+
+  } else if (threshold_is == "percentage"){
+
+    quantiles <- list("Accuracy" = quantile(by_observation[["Accuracy"]], probs = threshold))
+    to_return <- by_observation[by_observation[["Accuracy"]] < quantiles[["Accuracy"]],]
+
+  }
+
+  to_return %>%
+    dplyr::arrange(!!!rlang::syms(colnames(grouping_keys)),
+                   .data$Accuracy)
+
+}
+
+
+most_challenging_gaussian <- function(data,
+                                      obs_id_col,
+                                      target_col,
+                                      prediction_col,
+                                      threshold,
+                                      threshold_is,
+                                      grouping_keys){
+
+  if (!is.numeric(data[[target_col]]))
+    stop("target_col must be numeric.")
+  if (!is.numeric(data[[prediction_col]]))
+    stop("target_col must be numeric.")
+
+  tmp_residual_var <- create_tmp_var(data, ".__residuals__")
+  data[tmp_residual_var] <- data[[target_col]] - data[[prediction_col]]
+
+  root_mean_square <- function(x) sqrt(mean(x^2))
+
+  by_observation <- data %>%
+    dplyr::group_by_at(c(colnames(grouping_keys), obs_id_col)) %>%
+    dplyr::summarise(`MAE` = mean(abs(!!as.name(tmp_residual_var))),
+                     `RMSE` = root_mean_square(!!as.name(tmp_residual_var)))
+
+  if (threshold_is == "score"){
+
+    to_return <- by_observation[by_observation[["RMSE"]] > threshold,]
+
+  } else if (threshold_is == "percentage"){
+
+    quantiles <- list("RMSE" = quantile(by_observation[["RMSE"]], probs = (1-threshold)))
+
+    to_return <- by_observation[by_observation[["RMSE"]] > quantiles[["RMSE"]],]
+
+  }
+
+  to_return %>%
+    dplyr::arrange(!!!rlang::syms(colnames(grouping_keys)),
+                   desc(.data$RMSE), desc(.data$MAE))
 
 }
--- tests/testthat/test_most_challenging.R
+++ tests/testthat/test_most_challenging.R
@@ -23,7 +23,9 @@ test_that("binomial model works with most_challenging()",{
   collected_preds <- dplyr::bind_rows(CV_binom$Predictions, .id = "Model") %>%
     dplyr::group_by(.data$Model)
 
-  hard_to_predict <- most_challenging(collected_preds)
+  hard_to_predict <- most_challenging(collected_preds,
+                                      threshold = 0.25,
+                                      threshold_is = "percentage")
 
   expect_equal(hard_to_predict$Model,
                c("1", "1", "1", "1", "1", "1", "1", "2", "2", "2", "2", "2",
@@ -35,7 +37,22 @@ test_that("binomial model works with most_challenging()",{
                c(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L))
   expect_equal(hard_to_predict$Incorrect,
                c(8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L))
-  expect_equal(hard_to_predict$`Percentage Correct`,
+  expect_equal(hard_to_predict$Accuracy,
+               c(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L))
+
+  hard_to_predict <- most_challenging(collected_preds, threshold_is = "score")
+
+  expect_equal(hard_to_predict$Model,
+               c("1", "1", "1", "1", "1", "1", "1", "2", "2", "2", "2", "2",
+                 "2", "2"))
+  expect_equal(hard_to_predict$Observation,
+               c(1L, 4L, 5L, 7L, 10L, 20L, 21L, 1L, 4L, 7L, 10L, 20L, 21L, 30L
+               ))
+  expect_equal(hard_to_predict$Correct,
+               c(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L))
+  expect_equal(hard_to_predict$Incorrect,
+               c(8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L))
+  expect_equal(hard_to_predict$Accuracy,
                c(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L))
 
 })
@@ -81,22 +98,94 @@ test_that("multinomial model works with most_challenging()",{
   collected_preds <- dplyr::bind_rows(CVmultinomlist$Predictions, .id = "Model") %>%
     dplyr::group_by(.data$Model)
 
-  hard_to_predict <- most_challenging(collected_preds, threshold = 0.85)
+  hard_to_predict <- most_challenging(collected_preds, threshold = 0.15,
+                                      threshold_is = "percentage")
+
+  expect_equal(hard_to_predict$Model,
+               c("1", "1", "1", "1", "1", "1", "2", "2", "2"))
+  expect_equal(hard_to_predict$Observation,
+               c(4L, 10L, 11L, 12L, 17L, 35L, 5L, 33L, 50L))
+  expect_equal(hard_to_predict$Correct,
+               c(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L))
+  expect_equal(hard_to_predict$Incorrect,
+               c(8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L, 8L))
+  expect_equal(hard_to_predict$Accuracy,
+               c(0, 0, 0, 0, 0, 0, 0, 0, 0))
+
+  hard_to_predict <- most_challenging(collected_preds, threshold = 0.15, threshold_is = "score")
 
   expect_equal(hard_to_predict$Model,
                c("1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1",
                  "1", "1", "2", "2", "2"))
   expect_equal(hard_to_predict$Observation,
-               c(4L, 6L, 8L, 10L, 11L, 12L, 17L, 28L, 31L, 33L, 35L, 39L, 47L,
+               c(4L, 10L, 11L, 12L, 17L, 35L, 6L, 8L, 28L, 31L, 33L, 39L, 47L,
                  48L, 5L, 33L, 50L))
   expect_equal(hard_to_predict$Correct,
-               c(0L, 1L, 1L, 0L, 0L, 0L, 0L, 1L, 1L, 1L, 0L, 1L, 1L, 1L, 0L,
+               c(0L, 0L, 0L, 0L, 0L, 0L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 0L,
                  0L, 0L))
   expect_equal(hard_to_predict$Incorrect,
-               c(8L, 7L, 7L, 8L, 8L, 8L, 8L, 7L, 7L, 7L, 8L, 7L, 7L, 7L, 8L,
+               c(8L, 8L, 8L, 8L, 8L, 8L, 7L, 7L, 7L, 7L, 7L, 7L, 7L, 7L, 8L,
                  8L, 8L))
-  expect_equal(hard_to_predict$`Percentage Correct`,
-               c(0, 0.125, 0.125, 0, 0, 0, 0, 0.125, 0.125, 0.125, 0, 0.125,
+  expect_equal(hard_to_predict$Accuracy,
+               c(0, 0, 0, 0, 0, 0, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125,
                  0.125, 0.125, 0, 0, 0))
+
+})
+
+
+
+test_that("gaussian model works with most_challenging()",{
+
+  # Load data and fold it
+  set_seed_for_R_compatibility(1)
+  dat_ready <- participant.scores %>%
+    dplyr::mutate(diagnosis = as.factor(diagnosis))
+  dat_list <- groupdata2::fold(dat_ready,
+                               k = 4,
+                               num_fold_cols = 8,
+                               cat_col = 'diagnosis',
+                               id_col = 'participant')
+
+  CV_gauss <- cross_validate(
+    dat_list,
+    formulas = c("score ~ diagnosis + session", "score ~ diagnosis + age + session"),
+    fold_cols = paste0(".folds_", 1:8),
+    family = "gaussian"
+  )
+
+  collected_preds <- dplyr::bind_rows(CV_gauss$Predictions, .id = "Model") %>%
+    dplyr::group_by(.data$Model)
+
+  hard_to_predict <- most_challenging(collected_preds, type = "gaussian",
+                                      threshold = 0.1, threshold_is = "percentage")
+
+  expect_equal(hard_to_predict$Model,
+               c("1", "1", "1", "2", "2", "2"))
+  expect_equal(hard_to_predict$Observation,
+               c(20L, 24L, 21L, 20L, 24L, 21L))
+  expect_equal(hard_to_predict$MAE,
+               c(26.6104166666667, 20.9373511904762,
+                 18.2130952380952, 26.7693082340977,
+                 21.7422827228115, 18.3719868055263))
+  expect_equal(hard_to_predict$RMSE,
+               c(26.6125303350383, 20.9515642469683,
+                 18.2223366019013, 26.7757193125139,
+                 21.7845630990429, 18.3931551635641))
+
+  hard_to_predict <- most_challenging(collected_preds, type = "gaussian",
+                                      threshold = 13.9, threshold_is = "score")
+
+  expect_equal(hard_to_predict$Model,
+               c("1", "1", "1", "1", "2", "2", "2", "2", "2"))
+  expect_equal(hard_to_predict$Observation,
+               c(20L, 24L, 21L, 3L, 20L, 24L, 21L, 27L, 3L))
+  expect_equal(hard_to_predict$MAE,
+               c(26.6104166666667, 20.9373511904762, 18.2130952380952, 14.7703373015873,
+                 26.7693082340977, 21.7422827228115, 18.3719868055263, 13.4855783921144,
+                 13.8922545752059))
+  expect_equal(hard_to_predict$RMSE,
+               c(26.6125303350383, 20.9515642469683, 18.2223366019013, 14.7823170647799,
+                 26.7757193125139, 21.7845630990429, 18.3931551635641, 14.7146578087269,
+                 13.9530043384422))
 
 })

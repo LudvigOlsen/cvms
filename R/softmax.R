3488fd90cfb74b2b76ba5b19fe551a7075901989
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2020-01-25 19:02:50 +0100

    Improves and tests softmax()

--- R/softmax.R
+++ R/softmax.R
@@ -1,6 +1,69 @@
 #   __________________ #< d7bdb81fcb2355ed04f88471ceaffc0d ># __________________
 #   Softmax                                                                 ####
 
+##  .................. #< 65067b110e73ea523f84706701ea20c7 ># ..................
+##  Softmax function                                                        ####
+
+
+# axis : "r" for rowwise, "c" for column-wise
+softmax <- function(data, cols = NULL, axis = "r") {
+
+  # Check arguments ####
+  assert_collection <- checkmate::makeAssertCollection()
+  checkmate::assert_data_frame(x = data, add = assert_collection)
+  checkmate::assert_choice(x = axis, choices = c("r", "c"), add = assert_collection)
+  checkmate::reportAssertions(assert_collection)
+  if (!is.null(cols) && !is.numeric(cols)){
+    checkmate::assert_character(x = cols, null.ok = TRUE, add = assert_collection)
+    checkmate::assert_names(x = colnames(data), must.include = cols,
+                            type = "unique", what = "colnames",
+                            add = assert_collection)
+  } else {
+    checkmate::assert_integerish(x = cols, lower = 1, upper = ncol(data),
+                                 null.ok = TRUE, add = assert_collection)
+  }
+  checkmate::reportAssertions(assert_collection)
+  # End of argument checks ####
+
+  # Convert to tibble
+  data <- dplyr::as_tibble(data)
+
+  # Original order
+  col_order <- colnames(data)
+
+  # Set cols when not provided as character
+  if (is.null(cols)){
+    cols <- col_order
+  } else if (is.numeric(cols)) {
+    cols <- col_order[cols]
+  }
+
+  # Subset the data to process and to leave alone
+  data_to_process <- data[, cols]
+  data_to_leave <- data[, setdiff(colnames(data), cols)]
+
+  # Test that the probability columns are numeric
+  # TODO use checkmate to do the check?
+  if (!checkmate::test_data_frame(x = data_to_process, types = c("numeric"))) {
+    stop("softmax() only works on numeric columns.")
+  }
+
+  if (axis == "r"){
+    processed_data <- purrr::pmap_dfr(
+      data_to_process,
+      softmax_row
+    )
+  } else {
+    processed_data <- data_to_process %>%
+      dplyr::mutate_all(softmax_vector)
+  }
+
+  colnames(processed_data) <- cols
+  dplyr::bind_cols(processed_data, data_to_leave) %>%
+    base_select(cols = col_order) %>%
+    dplyr::as_tibble()
+}
+
 
 ##  .................. #< fb7b6d40446b03f933c7e0a3ae823a94 ># ..................
 ##  Log Sum Exp                                                             ####
@@ -17,14 +80,17 @@ logsumexp <- function(x) {
 
 
 softmax_row <- function(...) {
-  x <- unname(c(...))
-  x <- exp(x - logsumexp(x))
-  # Convert to row in tibble
-  # TODO There must be a better way
-  x <- dplyr::as_tibble(t(matrix(x)),
-                        .name_repair = ~ paste0("V", seq_len(length(x)))
-  )
-  x
+  arg_names <- non_empty_names(c(...))
+  x <- as.list(softmax_vector(...))
+
+  if (length(arg_names) == length(x)){
+    names(x) <- arg_names
+  } else {
+    names(x) <- paste0("V", seq_len(length(x)))
+  }
+
+  as.data.frame(x)
+
 }
 
 
@@ -37,44 +103,4 @@ softmax_vector <- function(...) {
   exp(x - logsumexp(x))
 }
 
-
-##  .................. #< 65067b110e73ea523f84706701ea20c7 ># ..................
-##  Softmax function                                                        ####
-
-
-# TODO Add tests of this !!!
-softmax <- function(data, cols = NULL) {
-
-  # Convert to tibble
-  data <- dplyr::as_tibble(data)
-
-  # TODO is this necessary?
-  if (!is.null(cols)) {
-    if (is.numeric(cols)) {
-      data_to_process <- data[, cols]
-      data_to_leave <- data[, setdiff(seq_len(ncol(data)), cols)]
-      cols <- colnames(data_to_process)
-    } else if (is.character(cols)) {
-      data_to_process <- data[, cols]
-      data_to_leave <- data[, setdiff(colnames(data), cols)]
-    }
-  } else {
-    data_to_process <- data
-    data_to_leave <- NULL
-    cols <- colnames(data)
-  }
-
-  # Test that the probability columns are numeric
-  # TODO use checkmate to do the check?
-  if (any(!sapply(data_to_process, is.numeric))) {
-    stop("softmax only works on numeric columns.")
-  }
-
-  processed_data <- purrr::pmap_dfr(
-    data_to_process,
-    softmax_row
-  )
-  colnames(processed_data) <- cols
-  dplyr::bind_cols(processed_data, data_to_leave)
-}
 

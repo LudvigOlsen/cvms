ed32755c479e50e58bf322044827981bd3b3526f
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2020-02-26 19:34:18 +0100

    Inits confusion matrix vignette

--- R/plot_confusion_matrix.R
+++ R/plot_confusion_matrix.R
@@ -552,19 +552,3 @@ plot_confusion_matrix <- function(conf_matrix,
 
   pl
 }
-
-
-# Finds path of figure in either /man/figures/ or /help/figures/
-# The latter being used in built packages
-get_figure_path <- function(fig_name){
-
-  package_path <- system.file(package="cvms")
-  for (sub in c("man","help")){
-    fig_path <- paste0(package_path, "/", sub, "/figures/", fig_name)
-    if (file.exists(fig_path))
-      return(fig_path)
-  }
-  warning("Could not find figure.")
-  invisible()
-
-}
